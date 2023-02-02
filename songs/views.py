59d05442cfda04b0e3e593fe95605ad32822d346
unknown <greateducation123@yahoo.com>
2023-02-02 12:03:58 -0600

    Queried and serialized in views and got an empty list in Postman

--- songs/views.py
+++ songs/views.py
@@ -1,8 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import SongSerializer
+from .models import Song
+
 
 @api_view(['GET'])
 def songs_list(request):
-    
 
-    return Response('ok')
+    songs = Song.objects.all()
+
+    serializer = SongSerializer(songs, many=True)
+
+    return Response(serializer.data)
+
+    
